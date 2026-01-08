3e7da43f6b58249ae04f7ad3bd123ca5e316be05
Aravind <91419047+Aravind320@users.noreply.github.com>
2026-01-08 15:28:32 +0000

    added code in practice.py

--- Day-11/practice.py
+++ Day-11/practice.py
@@ -1,3 +1,9 @@
-import request
+import requests
 
-url = f''
+response = requests.get('https://api.github.com/repos/kubernetes/kubernetes/pulls')
+
+complete_detail = response.json()
+
+for i in range(len(complete_detail)):
+    print(complete_detail[i]["user"]["login"])
+
