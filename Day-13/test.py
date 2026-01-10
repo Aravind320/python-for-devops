e639b4b3f910f8ed06bf4ae8d48b583e8d275c9e
Aravind <91419047+Aravind320@users.noreply.github.com>
2026-01-10 13:17:29 +0000

    adding s3 get acl

--- Day-13/test.py
+++ Day-13/test.py
@@ -2,6 +2,14 @@ import boto3
 
 client = boto3.client('s3')
 
-response = client.create_bucket(
+#response = client.create_bucket(
+#   Bucket='aravind-boto3-test-090',
+#)
+
+response = client.get_bucket_acl(
     Bucket='aravind-boto3-test-090',
 )
+
+grants = response['Grants']
+
+print(grants)
